a72ca88b80a868b0ef6b272f56a269b0039e25f9
ccadars <ccadars.beweb@gmail.com>
2017-02-16 16:52:52 +0100

    exercice securite

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,5 +16,33 @@ class DefaultController extends Controller
     {
         
         return $this->redirect("/blue");
+    }
+
+
+    /**
+     * @Route("/admin" , name="admin")
+     */
+    public function adminAction()
+    {
+        
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+    /**
+     * @Route("/logout" , name="logout")
+     */
+    public function logout(){
+        
+    }
+    /**
+     * @Route("/login" , name="login")
+     */
+    public function login(){
+        return $this->render('default/login.html.twig');
+    }
+    /**
+     * @Route("/loginCheck" , name="loginCheck")
+     */
+    public function loginCheck(){
+        
     }
 }
